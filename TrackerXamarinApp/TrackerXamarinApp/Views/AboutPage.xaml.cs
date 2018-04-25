4623e7551d9e4283efcd193ec7a46be40f2f1ebc
ankur blr <ankur.frend@gmail.com>
2018-04-25 16:25:15 +0530

    Initial commit for Tracker Xamarin App.

---
+++ TrackerXamarinApp/TrackerXamarinApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TrackerXamarinApp
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
